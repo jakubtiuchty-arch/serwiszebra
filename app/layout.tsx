dfd0d4cf3a7dcf9a7c24fb7f88d298aa3bd07bca
kuba <jakub.tiuchty@gmail.com>
2025-12-20 19:38:33 +0100

    chore: update Vercel Analytics import to @vercel/analytics/next

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { metadata as seoMetadata, viewport as seoViewport } from './metadata'
-import { Analytics } from '@vercel/analytics/react'
+import { Analytics } from '@vercel/analytics/next'
 
 const inter = Inter({
   subsets: ['latin'],
