3b39778ac76caf070587b9cfd0bcd8223c33c202
kuba <jakub.tiuchty@gmail.com>
2025-12-22 22:30:24 +0100

    Fix RSS feed: escape XML characters and encode image URLs

--- app/feed.xml/route.ts
+++ app/feed.xml/route.ts
@@ -1,22 +1,46 @@
 import { getAllPosts } from '@/lib/blog'
 
+// Funkcja do escape'owania znaków specjalnych XML
+function escapeXml(text: string): string {
+  return text
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&apos;')
+}
+
+// Funkcja do enkodowania URL (zachowuje strukturę ścieżki)
+function encodeImageUrl(url: string): string {
+  // Rozdziel ścieżkę na części i enkoduj każdą część osobno
+  const parts = url.split('/')
+  return parts.map(part => encodeURIComponent(part)).join('/')
+}
+
 export async function GET() {
   const posts = getAllPosts()
   const siteUrl = 'https://www.serwis-zebry.pl'
   
-  const rssItems = posts.map((post) => `
+  const rssItems = posts.map((post) => {
+    // Bezpieczne enkodowanie URL obrazu
+    const imageUrl = post.coverImage 
+      ? `${siteUrl}${encodeImageUrl(post.coverImage)}`
+      : null
+    
+    return `
     <item>
       <title><![CDATA[${post.title}]]></title>
       <link>${siteUrl}/blog/${post.slug}</link>
       <guid isPermaLink="true">${siteUrl}/blog/${post.slug}</guid>
       <description><![CDATA[${post.excerpt}]]></description>
       <pubDate>${new Date(post.publishedAt).toUTCString()}</pubDate>
-      <category>${post.category}</category>
-      <category>${post.deviceType}</category>
-      ${post.coverImage ? `<enclosure url="${siteUrl}${post.coverImage}" type="image/jpeg" />` : ''}
-      <author>serwis@serwis-zebry.pl (${post.author.name})</author>
+      <category>${escapeXml(post.category)}</category>
+      <category>${escapeXml(post.deviceType)}</category>
+      ${imageUrl ? `<enclosure url="${escapeXml(imageUrl)}" type="image/jpeg" length="0" />` : ''}
+      <author>serwis@serwis-zebry.pl (${escapeXml(post.author.name)})</author>
     </item>
-  `).join('')
+  `
+  }).join('')
 
   const rss = `<?xml version="1.0" encoding="UTF-8"?>
 <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:content="http://purl.org/rss/1.0/modules/content/">
